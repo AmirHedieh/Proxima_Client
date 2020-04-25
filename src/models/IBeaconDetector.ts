849b3ced27761ec5ad78bc2494e29ca2eef70005
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-04-25 21:26:02 +0430

    Refactor Beacon Detector

--- src/models/IBeaconDetector.ts
+++ src/models/IBeaconDetector.ts
@@ -1,9 +1,16 @@
+import { DeviceEventEmitter } from 'react-native'
 // @ts-ignore
 import Beacons from 'react-native-beacons-manager'
 import { EnvironmentVariables } from '../Constants'
 import { Logger } from '../utils/Logger'
-
-export class IBeaconDetector {
+import { BeaconFactory, IBeacon, IRawBeacon } from './Beacon'
+interface IBeaconDetector {
+    startDetecting: (region?: string) => Promise<boolean>
+    stopDetecting: (region?: string) => Promise<boolean>
+    init: (period: number) => void
+}
+export class BeaconDetector implements IBeaconDetector {
+    public onBeaconFetch: (data: IBeacon[]) => void = null
     constructor() {
         if (EnvironmentVariables.isIos) {
             this.configForIOS()
@@ -14,6 +21,7 @@ export class IBeaconDetector {
     public async startDetecting(region: string): Promise<boolean> {
         try {
             await Beacons.startRangingBeaconsInRegion(region)
+            DeviceEventEmitter.addListener('beaconsDidRange', (data) => this.onBeaconDidRange(data.beacons))
             return true
         } catch (err) {
             Logger.log(`Beacons ranging not started, error: ${err.message}`)
@@ -21,12 +29,29 @@ export class IBeaconDetector {
         return false
     }
     // TODO: ask if return type must be bool
-    public async stopDetecting(region: string): Promise<void> {
+    public async stopDetecting(region: string): Promise<boolean> {
         try {
             await Beacons.stopRangingBeaconsInRegion(region)
+            return true
         } catch (err) {
             Logger.log(`Beacons ranging not started, error: ${err.message}`)
         }
+        return false
+    }
+    public init(period: number): void {
+        Beacons.setForegroundScanPeriod(period)
+    }
+    public addListener(type: string, callback: () => void) {
+        DeviceEventEmitter.addListener(type, callback)
+    }
+    private onBeaconDidRange(data: IRawBeacon[]) {
+        Logger.log('on beacon did range called')
+        // parse data
+        const beacons: IBeacon[] = []
+        for (const element of data) {
+            beacons.push(BeaconFactory.generateBeacon(element))
+        }
+        this.onBeaconFetch(beacons)
     }
     // TODO: beacon manager package linking for IOS
     private configForIOS() {
