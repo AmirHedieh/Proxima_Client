08e9dd9d18015b961eebbcfe4672fb07fb097349
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-04-30 19:29:46 +0430

    Add detection state type

--- src/Types.ts
+++ src/Types.ts
@@ -3,22 +3,24 @@ export type LocalizationLanguages = 'en' | 'fa'
 export type StyleType = any
 
 export interface ISource {
-  uri: string
+    uri: string
 }
 
 export interface INavigation {
-  setParams: (arg: any) => void
-  state: {
-    params: any,
-    routeName: string
-  },
-  addListener: (event: string, callback: () => void) => INavigationAddListenerResult
+    setParams: (arg: any) => void
+    state: {
+        params: any
+        routeName: string
+    }
+    addListener: (event: string, callback: () => void) => INavigationAddListenerResult
 }
 
 export interface INavigationAddListenerResult {
-  remove: () => void
+    remove: () => void
 }
 
 export type LocalImage = number | { uri: string }
 
 export type LanguageKeyType = 'fa' | 'tr' | 'en'
+
+export type DetectionState = 'NO_STORE_NO_BEACON' | 'FOUND_STORE_NO_BEACON' | 'FOUND_STORE_FOUND_BEACON'
