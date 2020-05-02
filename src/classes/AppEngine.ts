b97d344d114496c486491fb3b95329c243e5ed38
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-05-02 14:02:31 +0430

    Add a rand generator to check navigation

--- src/classes/AppEngine.ts
+++ src/classes/AppEngine.ts
@@ -5,6 +5,7 @@ import { DetectionState } from '../Types'
 import { BeaconDetector, IBeaconDetector } from './BeaconDetector'
 import { BeaconEngine } from './BeaconEngine'
 import { INavigationHandler, NavigationHandler } from './NavigationHandler'
+import { RandomGenerator } from '../utils/RandomGenerator'
 
 export class AppEngine {
     @observable public products: Product[] = []
@@ -17,6 +18,7 @@ export class AppEngine {
 
     public constructor() {
         this.navHandler = new NavigationHandler()
+        // will be called just when detectionState changes not any other observable
         autorun(() => this.navHandler.navigate(this.detectionState))
         this.beaconDetector = new BeaconDetector()
         this.beaconEngine = new BeaconEngine(this.beaconDetector)
@@ -29,7 +31,11 @@ export class AppEngine {
                 const response = await HttpManager.getInstance().getProduct({ product: minor })
                 if (response.isSuccessful()) {
                     this.currentProduct = response.getData()
-                    this.detectionState = 'FOUND_STORE_FOUND_BEACON'
+                    if (RandomGenerator.generateRandomNumber(1, 100) % 2 === 0) {
+                        this.detectionState = 'FOUND_STORE_FOUND_BEACON'
+                    } else {
+                        this.detectionState = 'FOUND_STORE_NO_BEACON'
+                    }
                     console.log('product set')
                 }
             } catch (e) {
