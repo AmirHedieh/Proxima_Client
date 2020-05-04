56b87f5c7bd39b94e9afbb9207f08c147be9415b
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-05-04 05:40:05 +0430

    Replace  global state with mobx store for localization test

--- src/text_process/__tests__/LocalizationTest.ts
+++ src/text_process/__tests__/LocalizationTest.ts
@@ -1,19 +1,19 @@
-import {GlobalState} from '../../models/GlobalState'
+import { stores } from '../../mobx/RootStore'
 import { Localization } from '../Localization'
 
 describe('Localization', () => {
-  it('translate english with arg', () => {
-    expect(Localization.translate('test', ['moein'])).toEqual('Test moein')
-  })
-  it('translate english without arg', () => {
-    expect(Localization.translate('test')).toEqual('Test $arg0')
-  })
-  it('translate persian with arg', () => {
-    GlobalState.getInstance().setLanguage('fa')
-    expect(Localization.translate('test', ['معین'])).toEqual('تست معین')
-  })
-  it('translate persian without arg', () => {
-    GlobalState.getInstance().setLanguage('fa')
-    expect(Localization.translate('test')).toEqual('تست $arg0')
-  })
+    it('translate english with arg', () => {
+        expect(Localization.translate('test', ['moein'])).toEqual('Test moein')
+    })
+    it('translate english without arg', () => {
+        expect(Localization.translate('test')).toEqual('Test $arg0')
+    })
+    it('translate persian with arg', () => {
+        stores.UIState.changeLanguage('fa')
+        expect(Localization.translate('test', ['معین'])).toEqual('تست معین')
+    })
+    it('translate persian without arg', () => {
+        stores.UIState.changeLanguage('fa')
+        expect(Localization.translate('test')).toEqual('تست $arg0')
+    })
 })
