652f02d7f8e0629e305a796517b3fcc4005b5491
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-05-08 09:24:05 +0430

    Make safe touch style changable by children

--- src/components/base_dialog/BaseDialog.tsx
+++ src/components/base_dialog/BaseDialog.tsx
@@ -1,34 +1,32 @@
 import * as React from 'react'
-import {Modal, TouchableWithoutFeedback, View} from 'react-native'
+import { Modal, TouchableWithoutFeedback, View } from 'react-native'
 import { StyleType } from '../../Types'
 import { SafeTouch } from '../safe_touch/SafeTouch'
-import {Styles} from './BaseDialogStyles'
+import { Styles } from './BaseDialogStyles'
 
 // tslint:disable-next-line: no-empty-interface
-export interface IBaseDialogProps {
-
-}
+export interface IBaseDialogProps {}
 
 export interface IBaseDialogState {
-    isVisible: boolean,
-    isCancellable: boolean,
+    isVisible: boolean
+    isCancellable: boolean
 }
 
-export abstract class BaseDialog<PassedProps extends IBaseDialogProps, PassedState extends IBaseDialogState>
-      extends React.PureComponent<PassedProps, PassedState> {
-
+export abstract class BaseDialog<
+    PassedProps extends IBaseDialogProps,
+    PassedState extends IBaseDialogState
+> extends React.PureComponent<PassedProps, PassedState> {
     protected isModalTransparent: boolean = true
     protected centerContainerStyle: StyleType = null
+    protected safeTouchStyle: StyleType = null
 
     public constructor(props: PassedProps) {
         super(props)
         this.onRequestClose = this.onRequestClose.bind(this)
     }
     public render(): JSX.Element {
-        const centerContainerStyle = [
-            Styles.centerContainerStyle,
-            this.centerContainerStyle
-        ]
+        const centerContainerStyle = [Styles.centerContainerStyle, this.centerContainerStyle]
+        const safeTouchStyle = [Styles.safeTouchStyle, this.safeTouchStyle]
         return (
             <Modal // onDismiss is handled in onRequestClose function
                 animationType={'fade'}
@@ -37,19 +35,11 @@ export abstract class BaseDialog<PassedProps extends IBaseDialogProps, PassedSta
                 onShow={this.onShow} // Both platforms
                 onRequestClose={this.onRequestClose} // Android platform only
             >
-            <SafeTouch
-                style={Styles.safeTouchStyle}
-                activeOpacity={1}
-                onPress={this.onRequestClose}
-            >
-                <TouchableWithoutFeedback>
-                    <View
-                        style={centerContainerStyle}
-                    >
-                    {this.renderInside()}
-                    </View>
-                </TouchableWithoutFeedback>
-            </SafeTouch>
+                <SafeTouch style={safeTouchStyle} activeOpacity={1} onPress={this.onRequestClose}>
+                    <TouchableWithoutFeedback>
+                        <View style={centerContainerStyle}>{this.renderInside()}</View>
+                    </TouchableWithoutFeedback>
+                </SafeTouch>
             </Modal>
         )
     }
