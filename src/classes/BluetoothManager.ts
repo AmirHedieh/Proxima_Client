040c102f99f69936425245ba373c4c1b64740794
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-05-09 06:04:28 +0430

    Refactor BLEManager logic

--- src/classes/BluetoothManager.ts
+++ src/classes/BluetoothManager.ts
@@ -3,38 +3,27 @@ import { NativeEventEmitter, NativeModules } from 'react-native'
 import BleManager from 'react-native-ble-manager'
 import { EnvironmentVariables } from '../Constants'
 
-type BluetoothStateType = 'on' | 'off'
 type BleManagerBleState = 'on' | 'off' | 'turning_on' | 'turning_off'
 
 interface IBluetoothManager {
     enableAndroidBluetooth: (onSuccess?: () => void, onFail?: () => void) => void
-    isBluetoothEnabled: () => boolean
-    onBluetoothStateChange: (state: BluetoothStateType) => void
+    onBluetoothStateChange: (isEnabled: boolean) => void
 }
 
 export class BluetoothManager implements IBluetoothManager {
-    public onBluetoothStateChange: (state: BluetoothStateType) => void = null
-    private bluetoothState: BluetoothStateType = null
+    public onBluetoothStateChange: (isEnabled: boolean) => void = null
     private BleManagerModule = NativeModules.BleManager
     private bleManagerEmitter = new NativeEventEmitter(this.BleManagerModule)
 
     public async init() {
         try {
             await BleManager.start()
-            this.bleManagerEmitter.addListener('BleManagerDidUpdateState', (args: { state: BleManagerBleState }) => {
-                console.log(args.state)
-                this.setBluetoothState(args.state)
-                if (this.onBluetoothStateChange) {
-                    this.onBluetoothStateChange(this.bluetoothState)
-                }
-            })
+            this.bleManagerEmitter.addListener('BleManagerDidUpdateState', this.onBleStateChange)
             BleManager.checkState() // triggers the listener for once(called to set initial value)
         } catch (e) {
             console.log(e)
         }
     }
-
-    public isBluetoothEnabled = () => this.bluetoothState === 'on'
 
     public async enableAndroidBluetooth(onSuccess?: () => void, onFail?: () => void) {
         if (!EnvironmentVariables.isIos) {
@@ -52,11 +41,9 @@ export class BluetoothManager implements IBluetoothManager {
         }
     }
 
-    private setBluetoothState(state: BleManagerBleState): void {
-        if (state === 'on') {
-            this.bluetoothState = 'on'
-            return
+    private onBleStateChange = (args: { state: BleManagerBleState }): void => {
+        if (this.onBluetoothStateChange) {
+            this.onBluetoothStateChange(args.state === 'on')
         }
-        this.bluetoothState = 'off'
     }
 }
