a1ed267523a113b1b52240d282b2d4bb031914c6
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-05-10 03:04:58 +0430

    Refactor net manager

--- src/classes/NetManager.ts
+++ src/classes/NetManager.ts
@@ -1,12 +1,22 @@
 import NetInfo, { NetInfoState } from '@react-native-community/netinfo'
-interface INetManager {
+export interface INetManager {
+    subscribe: () => void
+    unSubscribe: () => void
     onConnectionStateChange: (isConnected: boolean) => void
 }
 export class NetManager implements INetManager {
     public onConnectionStateChange: (isConnected: boolean) => void = null
+    private unSubscriber: () => void = null
 
-    public init(): void {
-        NetInfo.addEventListener(this.onNetStateChange)
+    public subscribe(): void {
+        // net info runs listener once automatically thats why listener is not called manually here
+        this.unSubscriber = NetInfo.addEventListener(this.onNetStateChange)
+    }
+
+    public unSubscribe() {
+        if (this.unSubscriber) {
+            this.unSubscriber()
+        }
     }
 
     private onNetStateChange = (state: NetInfoState) => {
