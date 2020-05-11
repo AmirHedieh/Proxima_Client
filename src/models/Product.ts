e350e7e962d9f174f4f08370d620bca449dd2cd4
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-05-12 02:21:29 +0430

    Edit Product model

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,28 +1,32 @@
 export interface IRawProduct {
+    product: number
     name: string
-    minimalInfo: string
     info: string
     price: number
     bodyMaterial: string
     clothMaterial: string
     picture: string[]
+    active: boolean
+    category: number
 }
 
 export class Product {
+    public id: number
     public name: string
-    public minimalInfo: string
     public info: string
     public price: number
     public bodyMaterial: string
     public clothMaterial: string
     public picture: string[]
+    public category: number
     constructor(rawProduct: IRawProduct) {
+        this.id = rawProduct.product
         this.name = rawProduct.name
-        this.minimalInfo = rawProduct.minimalInfo
         this.info = rawProduct.info
         this.price = rawProduct.price
         this.bodyMaterial = rawProduct.bodyMaterial
         this.clothMaterial = rawProduct.clothMaterial
         this.picture = rawProduct.picture
+        this.category = rawProduct.category
     }
 }
