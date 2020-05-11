41428e15c1588f556c176bde7c7a2be891b18625
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-05-12 02:24:40 +0430

    Revise Store model

--- src/models/Store.ts
+++ src/models/Store.ts
@@ -1,12 +1,30 @@
 export interface IRawStore {
+    store: number
+    picture: string
     name: string
+    phone_number: string
+    whatsapp: string
+    instagram: string
+    telegram: string
     info: string
 }
 export class Store {
+    public store: number
+    public picture: string
     public name: string
+    public phoneNumber: string
+    public whatsapp: string
+    public instagram: string
+    public telegram: string
     public info: string
     public constructor(rawStore: IRawStore) {
+        this.store = rawStore.store
+        this.picture = rawStore.picture
         this.name = rawStore.name
+        this.phoneNumber = rawStore.phone_number
+        this.whatsapp = rawStore.whatsapp
+        this.instagram = rawStore.instagram
+        this.telegram = rawStore.telegram
         this.info = rawStore.info
     }
 }
