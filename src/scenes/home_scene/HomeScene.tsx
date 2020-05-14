9a274a790098a44977305af3ca1ddeefd9e2d0b2
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-05-14 12:17:56 +0430

    Add ExpandingTab to Homescene
    Add basic of renderering products in expanding tab

--- src/scenes/home_scene/HomeScene.tsx
+++ src/scenes/home_scene/HomeScene.tsx
@@ -1,6 +1,6 @@
 import { inject, observer } from 'mobx-react'
 import * as React from 'react'
-import { Linking, View } from 'react-native'
+import { FlatList, Linking, View } from 'react-native'
 // @ts-ignore
 import AndroidOpenSettings from 'react-native-android-open-settings'
 import { BluetoothManager, IBluetoothManager } from '../../classes/BluetoothManager'
@@ -8,10 +8,15 @@ import { DomainViewModel } from '../../classes/DomainViewModel'
 import { ILocationManager, LocationManager } from '../../classes/LocationManager'
 import { INetManager, NetManager } from '../../classes/NetManager'
 import { BaseText } from '../../components/base_text/BaseText'
+import { ExpandingTab } from '../../components/expanding_tab/ExpandingTab'
+import { NormalButton } from '../../components/normal_button/NormalButton'
 import { RequirementDialog } from '../../components/requirement_dialog/RequirementDialog'
 import { EnvironmentVariables } from '../../Constants'
+import { GlobalStyles } from '../../GlobalStyles'
+import { Product } from '../../models/Product'
 import { Localization } from '../../text_process/Localization'
 import { PermissionsHandler } from '../../utils/PermissionsHandler'
+import { RandomGenerator } from '../../utils/RandomGenerator'
 import { BaseScene } from '../base_scene/BaseScene'
 import { Styles } from './HomeSceneStyles'
 
@@ -71,9 +76,8 @@ export class HomeScene extends BaseScene<IHomeSceneProps, IHomeSceneState> {
 
     protected renderSafe(): JSX.Element {
         return (
-            <View>
+            <View style={{ width: '100%', height: '100%' }}>
                 <RequirementDialog ref={(ref) => (this.requirementDialog = ref)} />
-                <BaseText text={this.props.AppState.getDetectionState()} />
                 {this.renderContent()}
             </View>
         )
@@ -132,6 +136,7 @@ export class HomeScene extends BaseScene<IHomeSceneProps, IHomeSceneState> {
         this.requirementDialog.hide()
     }
     private renderContent(): JSX.Element {
+        return this.renderShowProducts()
         switch (this.props.AppState.getDetectionState()) {
             case 'NO_STORE_NO_BEACON':
                 return this.renderSearchingStore()
@@ -149,6 +154,43 @@ export class HomeScene extends BaseScene<IHomeSceneProps, IHomeSceneState> {
     }
 
     private renderShowProducts(): JSX.Element {
-        return <BaseText text={'list of products'} />
+        return (
+            <View style={{ flex: 1, backgroundColor: '#39f' }}>
+                <NormalButton
+                    text={'Add product'}
+                    onPress={() =>
+                        this.props.AppState.addProduct(
+                            new Product({
+                                product: RandomGenerator.generateRandomNumber(0, 1000),
+                                name: `Product ${RandomGenerator.generateRandomNumber(0, 1000)}`
+                            })
+                        )
+                    }
+                />
+                <View style={{ backgroundColor: '#f00', flexDirection: 'row', justifyContent: 'center' }}>
+                    <BaseText text={'LOGO'} />
+                    <View style={GlobalStyles.spacer} />
+                    <BaseText text={'Name'} />
+                </View>
+                <ExpandingTab collapsedTitle={'products'} expandedContent={this.renderProductList()} />
+            </View>
+        )
+    }
+
+    private renderProductList(): JSX.Element {
+        return (
+            <FlatList
+                data={this.props.AppState.getProductList().slice()}
+                renderItem={this.renderProductFlatListItem}
+                keyExtractor={Product.keyExtractor}
+                numColumns={2}
+            />
+        )
+    }
+
+    // it seems flatList item doesn't observe changes, so maybe it wont be reactive
+    private renderProductFlatListItem = (event: { item: Product }) => {
+        const product = event.item
+        return <BaseText text={product.name} />
     }
 }
