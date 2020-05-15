18b56880e3be7ac69fa2df2e90b7096070dbe18a
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-05-15 05:26:19 +0430

    Edit styles

--- src/components/expanding_tab/ExpandingTabStyles.ts
+++ src/components/expanding_tab/ExpandingTabStyles.ts
@@ -3,21 +3,18 @@ import { Colors } from '../../Constants'
 import { Dimension } from '../../GlobalStyles'
 
 export const Styles = StyleSheet.create({
-    collapsedContainer: {
+    container: {
         width: '100%',
-        height: 60,
-        position: 'absolute',
-        bottom: 0,
-        backgroundColor: Colors.pureWhite,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    expandedContainer: {
-        width: '100%',
-        height: Dimension.deviceHeight * 0.8,
         position: 'absolute',
         bottom: 0,
         backgroundColor: Colors.pureWhite,
         alignItems: 'center'
+    },
+    collapsedContainer: {
+        height: 60,
+        justifyContent: 'center'
+    },
+    expandedContainer: {
+        height: Dimension.deviceHeight * 0.8
     }
 })
