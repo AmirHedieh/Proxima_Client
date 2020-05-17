19ed5d0fab4c86fda3e38c787797312521f22629
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-05-17 14:29:21 +0430

    Add minimal product scene to App

--- src/scenes/minimal_product_scene/MinimalProductScene.tsx
+++ src/scenes/minimal_product_scene/MinimalProductScene.tsx
@@ -1,13 +1,28 @@
+import { inject, observer } from 'mobx-react'
 import * as React from 'react'
 import { View } from 'react-native'
+import { DomainViewModel } from '../../classes/DomainViewModel'
+import { BaseText } from '../../components/base_text/BaseText'
 import { NavigationActions } from '../../NavigationActions'
 import { BaseScene, IBaseSceneState } from '../base_scene/BaseScene'
 
-interface IMinimalProductSceneProps {}
+interface IMinimalProductSceneProps {
+    AppState: DomainViewModel
+    productId: number
+}
 
+@inject('AppState')
+@observer
 export class MinimalProductScene extends BaseScene<IMinimalProductSceneProps, IBaseSceneState> {
     public renderSafe(): JSX.Element {
-        return <View />
+        const product = this.props.AppState.getProductList()[this.props.productId]
+        return (
+            <View>
+                <BaseText text={product.id} />
+                <BaseText text={product.info} />
+                <BaseText text={product.name} />
+            </View>
+        )
     }
     protected onBackPress(): boolean {
         NavigationActions.pop()
