5c35745a27f245cf22151b94f04be1de51e54848
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-05-20 14:44:58 +0430

    Create product scene files

---
+++ src/scenes/product_scene/ProductScene.tsx
@@ -0,0 +1,31 @@
+import { inject, observer } from 'mobx-react'
+import * as React from 'react'
+import { View } from 'react-native'
+import { DomainViewModel } from '../../classes/DomainViewModel'
+import { BaseText } from '../../components/base_text/BaseText'
+import { NavigationActions } from '../../NavigationActions'
+import { BaseScene, IBaseSceneState } from '../base_scene/BaseScene'
+
+interface IProductSceneProps {
+    AppState: DomainViewModel
+    productId: number
+}
+
+@inject('AppState')
+@observer
+export class ProductScene extends BaseScene<IProductSceneProps, IBaseSceneState> {
+    public renderSafe(): JSX.Element {
+        const product = this.props.AppState.getCurrentProduct()
+        return (
+            <View>
+                <BaseText text={product.id} />
+                <BaseText text={product.info} />
+                <BaseText text={product.name} />
+            </View>
+        )
+    }
+    protected onBackPress(): boolean {
+        NavigationActions.pop()
+        return true
+    }
+}
