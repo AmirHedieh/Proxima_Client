fe0ae9fa7b088d7608ae46ed33fa96f6e764d25d
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-05-23 08:25:29 +0430

    Add parent property

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,15 +1,18 @@
 export interface IRawCategory {
     category: number
+    parent: number
     name: string
     children: number[]
 }
 
 export class Category {
     public id: number
+    public parent: number
     public name: string
     public children: number[]
     public constructor(rawCategory: IRawCategory) {
         this.id = rawCategory.category
+        this.parent = rawCategory.parent
         this.name = rawCategory.name
         this.children = rawCategory.children
     }
