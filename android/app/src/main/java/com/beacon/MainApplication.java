46a9d3d8dc11dd6eb6c46457f1c2b8e4e5150850
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-05-23 15:58:17 +0430

    Add and Link fonts

--- android/app/src/main/java/com/beacon/MainApplication.java
+++ android/app/src/main/java/com/beacon/MainApplication.java
@@ -3,6 +3,7 @@ package com.beacon;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.nearit.connectivity.RNConnectivityStatusPackage;
 import com.levelasquez.androidopensettings.AndroidOpenSettingsPackage;
 import it.innove.BleManagerPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RNConnectivityStatusPackage(),
             new AndroidOpenSettingsPackage(),
             new BleManagerPackage(),
