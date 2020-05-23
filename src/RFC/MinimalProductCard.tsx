a320c4dc1cc7818a935ccbe48bf3d53df14a9523
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-05-23 22:10:51 +0430

    Pass image prop for image source

--- src/RFC/MinimalProductCard.tsx
+++ src/RFC/MinimalProductCard.tsx
@@ -14,7 +14,7 @@ interface IProductCardProps {
 export const MinimalProductCard: React.FunctionComponent<IProductCardProps> = (props: IProductCardProps) => {
     return (
         <SafeTouch onPress={props.onPress} style={Styles.root}>
-            <Image style={Styles.image} source={{ uri: 'https://i.picsum.photos/id/826/200/200.jpg' }} />
+            <Image style={Styles.image} source={{ uri: props.image }} />
             <View style={Styles.bottomContainer}>
                 <BaseText style={Styles.title} text={props.title} />
                 <BaseText style={Styles.price} text={`${props.price} تومان`} />
