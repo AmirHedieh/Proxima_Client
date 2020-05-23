79bf07836059414774bd8370c3784f433001b399
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-05-23 10:56:00 +0430

    Fix BaseScene root style

--- src/scenes/base_scene/BaseSceneStyles.ts
+++ src/scenes/base_scene/BaseSceneStyles.ts
@@ -1,14 +1,9 @@
-import {
-    StyleSheet
-} from 'react-native'
+import { StyleSheet } from 'react-native'
 import { Colors } from '../../Constants'
 
 export const Styles = StyleSheet.create({
     root: {
-      flex: 1,
-      alignContent: 'center',
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: Colors.white
+        flex: 1,
+        backgroundColor: Colors.white
     }
 })
