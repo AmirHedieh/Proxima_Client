cec030d18e9af9078e717a87f72c689d18623089
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-05-30 12:17:38 +0430

    Fix initial version

--- src/Constants.ts
+++ src/Constants.ts
@@ -9,7 +9,7 @@ export const EnvironmentVariables = {
     isIPhoneX: window.width === 812 || window.height === 812 || window.width === 896 || window.height === 896,
     defaultDbName: '@mivanet_storage',
     isFakeDataMode: true,
-    version: '1.0.0'
+    version: '1.0'
 }
 
 export const Colors = {
@@ -43,9 +43,9 @@ export const Colors = {
 }
 
 export const NetworkConfig = {
-    apiBaseUrl: 'http://192.168.1.9:3000/',
+    apiBaseUrl: 'http://192.168.1.7:3000/',
     maxRetry: 3,
     httpRequestTimeout: 5000,
     maxConcurrentConnections: 1,
-    localServerPictureBaseUrl: 'http://185.8.173.206:80/picture'
+    localServerPictureBaseUrl: 'http://192.168.1.7:80/picture'
 }
