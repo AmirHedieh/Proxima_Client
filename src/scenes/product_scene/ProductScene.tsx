87ed027edc3c41d76decc9a014773d533bd0d5b1
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-05-31 20:09:05 +0430

    Add Basic scene temaplate

--- src/scenes/product_scene/ProductScene.tsx
+++ src/scenes/product_scene/ProductScene.tsx
@@ -1,12 +1,15 @@
 import { inject, observer } from 'mobx-react'
 import * as React from 'react'
-import { View } from 'react-native'
-import { DomainViewModel } from '../../classes/DomainViewModel'
+import { ImageBackground, View } from 'react-native'
+import { BackButton } from '../../components/back_button/BackButton'
 import { BaseText } from '../../components/base_text/BaseText'
 import { NavigationActions } from '../../NavigationActions'
+import { Localization } from '../../text_process/Localization'
 import { BaseScene, IBaseSceneState } from '../base_scene/BaseScene'
+import { Styles } from './ProductSceneStyles'
+import { DomainViewModel } from '../../classes/DomainViewModel'
 
-interface IProductSceneProps {
+export interface IProductSceneProps {
     AppState?: DomainViewModel
 }
 
@@ -14,16 +17,26 @@ interface IProductSceneProps {
 @observer
 export class ProductScene extends BaseScene<IProductSceneProps, IBaseSceneState> {
     public renderSafe(): JSX.Element {
-        const product = this.props.AppState.getCurrentProduct()
         return (
-            <View>
-                <BaseText text={'Found beacon!'} />
-                <BaseText text={`id: ${product?.id}`} />
-                <BaseText text={`name: ${product?.productName}`} />
+            <View style={Styles.root}>
+                <ImageBackground
+                    style={Styles.image}
+                    source={{ uri: this.props.AppState.getCurrentProduct().picture[0] }}
+                >
+                    <BackButton style={Styles.backButton} onPress={this.onBackPress} />
+                </ImageBackground>
+                <View style={Styles.bottomContainer}>
+                    <BaseText style={Styles.name} text={this.props.AppState.getCurrentProduct().productName} />
+                    <BaseText
+                        style={Styles.price}
+                        text={`${this.props.AppState.getCurrentProduct().price} ${Localization.translate('moneyUnit')}`}
+                    />
+                </View>
             </View>
         )
     }
-    protected onBackPress(): boolean {
+
+    protected onBackPress() {
         NavigationActions.pop()
         return true
     }
--- src/scenes/product_scene/ProductSceneStyles.ts
+++ src/scenes/product_scene/ProductSceneStyles.ts
@@ -1,3 +1,34 @@
 import { StyleSheet } from 'react-native'
+import { Colors } from '../../Constants'
+import { Dimension, Fonts, FontSizes } from '../../GlobalStyles'
 
-export const Styles = StyleSheet.create({})
+export const Styles = StyleSheet.create({
+    root: {
+        flex: 1,
+        backgroundColor: Colors.milky
+    },
+    backButton: {
+        position: 'absolute',
+        left: 20,
+        top: 16
+    },
+    image: {
+        width: '100%',
+        height: 270 * Dimension.scaleX
+    },
+    bottomContainer: {
+        flex: 1,
+        alignItems: 'center',
+        paddingTop: 40 * Dimension.scaleX,
+        paddingBottom: 32 * Dimension.scaleX,
+        paddingHorizontal: 28 * Dimension.scaleX
+    },
+    name: {
+        fontSize: FontSizes.h1,
+        fontFamily: Fonts.persian.vazirBold
+    },
+    price: {
+        fontSize: FontSizes.h3,
+        color: Colors.creamMedium2
+    }
+})
