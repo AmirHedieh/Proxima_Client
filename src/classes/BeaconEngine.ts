dfaf2943fe2b04a3257a289ac052ab34ec9e0f4e
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-06-01 15:08:46 +0430

    Implement beacon engine major - minor process loops

--- src/classes/BeaconEngine.ts
+++ src/classes/BeaconEngine.ts
@@ -2,6 +2,10 @@ import { IBeacon } from '../models/Beacon'
 import { IBeaconDetector } from './BeaconDetector'
 import { RepeatDetector } from './RepeatDetector'
 
+interface IMajorHistogram {
+    major: number
+    repeat: number
+}
 export class BeaconEngine {
     public onMajorChange: (major: number) => void = null
     public onMinorChange: (minor: number) => void = null
@@ -9,11 +13,13 @@ export class BeaconEngine {
     private beacons: IBeacon[] = []
     private major: number = -1
     private minor: number = -1
+    private majorRepeatDetector: RepeatDetector
     private minorRepeatDetector: RepeatDetector
 
     public constructor(beaconDetector: IBeaconDetector) {
         this.beaconDetector = beaconDetector
-        this.minorRepeatDetector = new RepeatDetector(3)
+        this.majorRepeatDetector = new RepeatDetector(4)
+        this.minorRepeatDetector = new RepeatDetector(4)
     }
 
     public getActiveMajor(): number {
@@ -43,36 +49,78 @@ export class BeaconEngine {
     }
 
     private processLoop(): void {
-        console.log(this.beacons)
-        for (const element of this.beacons) {
-            console.log(`minor: ${element.minor} rssi: ${element.rssi}`)
-        }
-        if (this.beacons.length === 0) {
-            return
-        }
-        const closestBeacon = this.findClosestBeacon()
-        this.minorRepeatDetector.addToData(closestBeacon.minor)
-        if (this.onMajorChange) {
-            if (this.major !== this.beacons[0].major) {
-                this.major = this.beacons[0].major
-                this.onMajorChange(this.beacons[0].major)
+        const sortedBeacons: IBeacon[] = this.sortByRSSI(this.beacons)
+        console.log(sortedBeacons)
+        const majorHistogram = this.majorHistogram(sortedBeacons)
+        const sortedHistogram = majorHistogram.sort((a, b) => b.repeat - a.repeat)
+        this.processMajor(sortedHistogram, sortedBeacons)
+        this.processMinor(sortedBeacons)
+    }
+
+    private processMajor(sortedHistogram: IMajorHistogram[], sortedBeacons: IBeacon[]) {
+        if (sortedHistogram.length === 0 || Math.abs(sortedBeacons[0].rssi) > 85) {
+            this.majorRepeatDetector.addToData(null)
+        } else if (sortedHistogram.length === 1) {
+            this.majorRepeatDetector.addToData(sortedHistogram[0].major)
+        } else {
+            if (sortedHistogram[0].repeat === sortedHistogram[1].repeat) {
+                this.majorRepeatDetector.addToData(sortedBeacons[0].major)
+            } else {
+                this.majorRepeatDetector.addToData(sortedHistogram[0].major)
             }
         }
-        // TODO: prevent to invoke minor change when closest beacon is what was chosen last time as well
-        if (this.minorRepeatDetector.isDataRepeated()) {
-            if (this.onMinorChange) {
-                this.onMinorChange(closestBeacon.minor)
+        if (this.majorRepeatDetector.isDataRepeated().isRepeated) {
+            if (this.major !== this.majorRepeatDetector.isDataRepeated().repeatedValue) {
+                if (this.onMajorChange) {
+                    this.major = this.majorRepeatDetector.isDataRepeated().repeatedValue
+                    // console.log('on major change called', this.major)
+                    this.onMajorChange(this.major)
+                }
             }
         }
     }
 
-    private findClosestBeacon(): IBeacon {
-        let closestBeacon = this.beacons[0]
-        for (const element of this.beacons) {
-            if (Math.abs(element.rssi) <= Math.abs(closestBeacon.rssi)) {
-                closestBeacon = element
+    private processMinor(sortedBeacons: IBeacon[]): void {
+        console.log(sortedBeacons)
+        if (sortedBeacons.length === 0 || Math.abs(sortedBeacons[0].rssi) > 70) {
+            this.minorRepeatDetector.addToData(null)
+        } else {
+            this.minorRepeatDetector.addToData(sortedBeacons[0].minor)
+        }
+        if (this.minorRepeatDetector.isDataRepeated().isRepeated) {
+            if (this.minor !== this.minorRepeatDetector.isDataRepeated().repeatedValue) {
+                if (this.onMinorChange) {
+                    this.minor = this.minorRepeatDetector.isDataRepeated().repeatedValue
+                    this.onMinorChange(this.minor)
+                }
             }
         }
-        return closestBeacon
+    }
+
+    private majorHistogram(beacons: IBeacon[]) {
+        const histogramList: IMajorHistogram[] = []
+        const temp: any[] = []
+        for (const beacon of beacons) {
+            const major = beacon.major
+            if (!temp[major]) {
+                let repeat = 0
+                for (const element of beacons) {
+                    if (element.major === major) {
+                        repeat++
+                    }
+                }
+                histogramList.push({
+                    major,
+                    repeat
+                })
+                // add a value for this major to prevent re-entering for
+                temp[major] = 1
+            }
+        }
+        return histogramList
+    }
+
+    private sortByRSSI(beacons: IBeacon[]) {
+        return beacons.sort((a, b) => b.rssi - a.rssi).slice(0, 4)
     }
 }
