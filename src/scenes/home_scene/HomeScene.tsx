cf17175999dfe189201873d5fbb54bf57ea2bd57
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-06-14 14:22:08 +0430

    Remove dialog

--- src/scenes/home_scene/HomeScene.tsx
+++ src/scenes/home_scene/HomeScene.tsx
@@ -1,16 +1,7 @@
 import { inject, observer } from 'mobx-react'
 import * as React from 'react'
-import { BackHandler, Linking, View } from 'react-native'
-// @ts-ignore
-import AndroidOpenSettings from 'react-native-android-open-settings'
-import { BluetoothManager, IBluetoothManager } from '../../classes/BluetoothManager'
+import { View } from 'react-native'
 import { DomainViewModel } from '../../classes/DomainViewModel'
-import { ILocationManager, LocationManager } from '../../classes/LocationManager'
-import { INetManager, NetManager } from '../../classes/NetManager'
-import { RequirementDialog } from '../../components/requirement_dialog/RequirementDialog'
-import { EnvironmentVariables } from '../../Constants'
-import { Localization } from '../../text_process/Localization'
-import { PermissionsHandler } from '../../utils/PermissionsHandler'
 import { BaseScene } from '../base_scene/BaseScene'
 import { ProductScene } from '../product_scene/ProductScene'
 import { StoreInfoScene } from '../store_info_scene/StoreInfoScene'
@@ -24,120 +15,18 @@ interface IHomeSceneState {}
 @inject('AppState')
 @observer
 export class HomeScene extends BaseScene<IHomeSceneProps, IHomeSceneState> {
-    private netManager: INetManager = null
-    private bluetoothManager: IBluetoothManager = null
-    private locationManager: ILocationManager = null
-
-    private requirementDialog: RequirementDialog = null
-    private requirements = {
-        isConnected: true,
-        isBluetoothOn: true,
-        isLocationOn: true
-    }
-
-    public constructor(props) {
-        super(props)
-
-        this.netManager = new NetManager()
-        this.netManager.onConnectionStateChange = (isConnected) => {
-            this.requirements.isConnected = isConnected
-            this.checkRequirements()
-        }
-
-        this.bluetoothManager = new BluetoothManager()
-        this.bluetoothManager.onBluetoothStateChange = (isEnabled) => {
-            this.requirements.isBluetoothOn = isEnabled
-            this.checkRequirements()
-        }
-
-        this.locationManager = new LocationManager()
-        this.locationManager.onLocationStatusChange = (isEnabled) => {
-            this.requirements.isLocationOn = isEnabled
-            this.checkRequirements()
-        }
-    }
-
-    protected async sceneDidMount() {
-        this.netManager.subscribe() // event get called on subscribe once
-        await this.bluetoothManager.subscribe() // event get called on subscribe once
-        await this.locationManager.subscribe() //
-        this.checkRequirements()
-    }
-
     protected async sceneWillUnmount() {
-        this.netManager.unSubscribe()
-        this.bluetoothManager.unSubscribe()
-        this.locationManager.unSubscribe()
-
         await this.props.AppState.stopDetecting()
     }
 
     protected renderSafe(): JSX.Element {
-        return (
-            <View style={{ flex: 1 }}>
-                <RequirementDialog ref={(ref) => (this.requirementDialog = ref)} />
-                {this.renderContent()}
-            </View>
-        )
+        return <View style={{ flex: 1 }}>{this.renderContent()}</View>
     }
     protected onBackPress(): boolean {
         BackHandler.exitApp()
         return true
     }
-    private async checkRequirements() {
-        if (this.requirements.isConnected === false) {
-            this.requirementDialog.show({
-                message: Localization.translate('connectionErrorHomeScene'),
-                icon: 'wifi'
-            })
-            return
-        }
-        if (this.requirements.isBluetoothOn === false) {
-            const buttonText = EnvironmentVariables.isIos
-                ? ''
-                : Localization.translate('enableAndroidBluetoothHomeScene')
-            this.requirementDialog.show({
-                message: Localization.translate('bluetoothErrorHomeScene'),
-                icon: 'bluetooth',
-                buttonText,
-                onButtonPressedCallback: () => {
-                    // button is only appeared in android
-                    this.bluetoothManager.enableAndroidBluetooth()
-                }
-            })
-            return
-        }
-        if ((await PermissionsHandler.isLocationPermissionAllowed()) === false) {
-            this.requirementDialog.show({
-                message: Localization.translate('locationPermissionErrorHomeScene'),
-                icon: 'location-on',
-                buttonText: Localization.translate('giveLocationPermissionHomeScene'),
-                onButtonPressedCallback: async () => {
-                    await PermissionsHandler.requestLocationPermission()
-                    this.checkRequirements()
-                }
-            })
-            return
-        }
-        if (this.requirements.isLocationOn === false) {
-            // TODO: check if opening setting works for ios, if not change the flow
-            const buttonText = Localization.translate('goToLocationSettingHomeScene')
-            this.requirementDialog.show({
-                message: Localization.translate('locationErrorHomeScene'),
-                icon: 'location-on',
-                buttonText,
-                onButtonPressedCallback: () => {
-                    if (EnvironmentVariables.isIos) {
-                        Linking.openURL('App-Prefs:LOCATION_SERVICES')
-                    } else {
-                        AndroidOpenSettings.locationSourceSettings()
-                    }
-                }
-            })
-            return
-        }
-        this.requirementDialog.hide()
-    }
+
     private renderContent(): JSX.Element {
         // return this.renderCurrentProduct()
         switch (this.props.AppState.getDetectionState()) {
