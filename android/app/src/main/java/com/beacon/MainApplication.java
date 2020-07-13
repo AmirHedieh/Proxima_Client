6508279176c6ce324a03ef6589c062722972315d
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-07-14 02:20:36 +0430

    Add and link fb messaging package

--- android/app/src/main/java/com/beacon/MainApplication.java
+++ android/app/src/main/java/com/beacon/MainApplication.java
@@ -3,6 +3,7 @@ package com.beacon;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.messaging.ReactNativeFirebaseMessagingPackage;
 import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.nearit.connectivity.RNConnectivityStatusPackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeFirebaseMessagingPackage(),
             new ReactNativeFirebaseAppPackage(),
             new VectorIconsPackage(),
             new RNConnectivityStatusPackage(),
