5bcbc95f76696961e4d75984c0332fe7f4dec4a9
Amirhossein-Hediehloo <amirhosseinhediehloo@gmail.com>
2020-07-15 18:35:56 +0430

    Add Headless check

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 /**
  * @format
  */
+import * as React from 'react'
+import { AppRegistry } from 'react-native'
+import { App } from './src/App'
+import { name as appName } from './app.json'
 
-import { AppRegistry } from "react-native";
-import { App } from "./src/App";
-import { name as appName } from "./app.json";
+// safety for side-effects in notification in background mode
+function HeadlessCheck({ isHeadless }) {
+    if (isHeadless) {
+        // App has been launched in the background by iOS, ignore
+        return null
+    }
 
-AppRegistry.registerComponent(appName, () => App);
+    return <App />
+}
+
+AppRegistry.registerComponent(appName, () => HeadlessCheck)
