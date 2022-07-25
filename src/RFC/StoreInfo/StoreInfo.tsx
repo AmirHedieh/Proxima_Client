fb87cb202aba732dcd2715a19240f0b9c7cc5d3d
Amirhossein Hediehloo <amirhosseinhediehloo@gmail.com>
2022-07-25 11:31:41 +0430

    Update storeinfo card

--- src/RFC/StoreInfo/StoreInfo.tsx
+++ src/RFC/StoreInfo/StoreInfo.tsx
@@ -1,31 +1,69 @@
 import * as React from 'react'
-import { Image, ScrollView, View } from 'react-native'
+import { ImageBackground, Linking, ScrollView, View } from 'react-native'
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome'
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 import { BaseText } from '../../components/base_text/BaseText'
-import { NetworkConfig } from '../../Constants'
+import { RoundButton } from '../../components/round_button/RoundButton'
+import { RTLAwareView } from '../../components/rtl_aware/RTLAwareView'
+import { SafeTouch } from '../../components/safe_touch/SafeTouch'
+import { Colors, NetworkConfig } from '../../Constants'
 import { Dimension, GlobalStyles } from '../../GlobalStyles'
-import { IStore } from '../../models/Store'
+import { IMuseum } from '../../models/Museum'
+import { SceneParams } from '../../SceneParams'
+import { Localization } from '../../text_process/Localization'
 import { Styles } from './StoreInfoStyles'
 
 const infoIconSize: number = 29 * Dimension.scaleX
 
-export const StoreInfo: React.FunctionComponent<IStore> = (props: IStore): JSX.Element => {
+interface IProps extends IMuseum {
+}
+export const StoreInfo: React.FunctionComponent<IProps> = (props: IProps): JSX.Element => {
+    const onWebsitePress = () => {
+        const url = `https://${props.website}`
+        Linking.canOpenURL(url).then(supported => {
+          if (supported) {
+            Linking.openURL(url);
+          } else {
+            console.warn("Don't know how to open URI: " + url);
+          }
+        });
+    }
+    
+    const onInstagramPress = () => {
+        Linking.openURL(`instagram://user?username=${(props.instagram)}`)
+        .catch(() => {
+            Linking.openURL(`https://www.instagram.com/${(props.instagram)}`);
+        })
+    }
+    
+    const onTwitterPress = () => {
+        Linking.openURL(`twitter://user?screen_name=${props.twitter}`)
+        .catch(() => {
+            Linking.openURL(`https://www.twitter.com/${props.twitter}`);
+        })
+    }
+
     return (
         <ScrollView contentContainerStyle={Styles.centerScrollViewContainer}>
-            <BaseText style={Styles.name} text={props.storeName} />
+            <BaseText style={Styles.name} text={props.name} />
 
             <View style={Styles.mediumSpacer} />
 
-            <Image style={Styles.image} source={{ uri: NetworkConfig.localServerPictureBaseUrl + props.picture }} />
+            <ImageBackground
+                style={Styles.image}
+                source={{ uri: NetworkConfig.localServerPictureBaseUrl + props.picture }}
+            >
+                {props.picture ? null : <View style={GlobalStyles.spacer}/>}
+                {props.picture ? null : <BaseText style={Styles.noImageText} text={Localization.translate('noPictureStoreInfoScene')}/>}
+            </ImageBackground>
 
             <View style={Styles.largeSpacer} />
 
             <View style={Styles.rowCenterView}>
                 <MaterialIcon name='home' size={infoIconSize} />
                 <View style={GlobalStyles.spacer} />
-                <BaseText text={props.address} />
+                <BaseText text={props.name} />
             </View>
 
             <View style={Styles.smallSpacer} />
@@ -38,36 +76,42 @@ export const StoreInfo: React.FunctionComponent<IStore> = (props: IStore): JSX.E
 
             <View style={Styles.mediumSpacer} />
 
-            <BaseText style={Styles.info} text={props.info} />
+            <View style={Styles.informationContainer}>
+                <RTLAwareView>
+                    <BaseText style={Styles.infoTitle} text={Localization.translate('informationStoreInfoScene')} />
+                    <View style={GlobalStyles.spacer}/>
+                </RTLAwareView>
+                <BaseText style={Styles.info} text={props.information} />
+            </View>
 
             <View style={Styles.mediumSpacer} />
 
-            {props.whatsapp && (
-                <View style={Styles.rowCenterView}>
-                    <FontAwesomeIcon name='whatsapp' size={infoIconSize} />
+            {props.website && (
+                <SafeTouch onPress={onWebsitePress} style={Styles.rowCenterView}>
+                    <FontAwesomeIcon name='globe' size={infoIconSize} />
                     <View style={GlobalStyles.spacer} />
-                    <BaseText style={Styles.contactText} text={props.whatsapp} />
-                </View>
+                    <BaseText style={Styles.contactText} text={props.website} />
+                </SafeTouch>
             )}
 
             <View style={Styles.smallSpacer} />
 
             {props.instagram && (
-                <View style={Styles.rowCenterView}>
+                <SafeTouch onPress={onInstagramPress} style={Styles.rowCenterView}>
                     <FontAwesomeIcon name='instagram' size={infoIconSize} />
                     <View style={GlobalStyles.spacer} />
-                    <BaseText style={Styles.contactText} text={props.instagram} />
-                </View>
+                    <BaseText style={Styles.contactText} text={`@${props.instagram}`} />
+                </SafeTouch>
             )}
 
             <View style={Styles.smallSpacer} />
 
-            {props.telegram && (
-                <View style={Styles.rowCenterView}>
-                    <FontAwesome5 name='telegram-plane' size={infoIconSize} />
+            {props.twitter && (
+                <SafeTouch onPress={onTwitterPress} style={Styles.rowCenterView}>
+                    <FontAwesome5 name='twitter' size={infoIconSize} />
                     <View style={GlobalStyles.spacer} />
-                    <BaseText style={Styles.contactText} text={props.telegram} />
-                </View>
+                    <BaseText style={Styles.contactText} text={`@${props.twitter}`} />
+                </SafeTouch>
             )}
         </ScrollView>
     )
--- src/RFC/StoreInfo/StoreInfoStyles.ts
+++ src/RFC/StoreInfo/StoreInfoStyles.ts
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native'
+import { Colors } from '../../Constants'
 import { Dimension, Fonts, FontSizes } from '../../GlobalStyles'
 
 export const Styles = StyleSheet.create({
@@ -13,22 +14,56 @@ export const Styles = StyleSheet.create({
         fontFamily: Fonts.persian.vazirMedium
     },
     rowCenterView: {
+        borderColor: Colors.creamDark,
+        borderWidth: 2 * Dimension.scaleX,
+        borderRadius: 8 * Dimension.scaleX,
         flexDirection: 'row',
         alignItems: 'center',
         paddingHorizontal: 16 * Dimension.scaleX,
         paddingVertical: 8 * Dimension.scaleY
     },
+    noImageText: {
+        fontSize: FontSizes.h1,
+        color: Colors.creamDark,
+        flex: 1
+    },
     image: {
         width: 304 * Dimension.scaleX,
         height: 228 * Dimension.scaleY,
-        borderRadius: 12 * Dimension.scaleX
+        alignItems: 'center',
+        borderRadius: 12 * Dimension.scaleX,
+        overflow: 'hidden',
+        backgroundColor: Colors.creamLight
+    },
+    informationContainer: {
+        width: '100%',
+        // padding: 16 * Dimension.scaleX,
+        // borderRadius: 16 * Dimension.scaleX,
+        // backgroundColor: Colors.creamLight
     },
     phoneNumber: {
         fontFamily: Fonts.persian.vazirBold,
         fontSize: FontSizes.h2
     },
+    infoTitle: {
+        paddingHorizontal: 16 * Dimension.scaleX,
+        paddingTop: 8 * Dimension.scaleX,
+        paddingBottom: 4 * Dimension.scaleX,
+        borderTopColor: Colors.creamDark,
+        borderLeftColor: Colors.creamDark,
+        borderRightColor: Colors.creamDark,
+        borderTopWidth: 2 * Dimension.scaleX,
+        borderLeftWidth: 2 * Dimension.scaleX,
+        borderRightWidth: 2 * Dimension.scaleX,
+        borderTopLeftRadius:16 * Dimension.scaleX,
+        borderTopRightRadius:16 * Dimension.scaleX
+    },
     info: {
-        width: '100%'
+        width: '100%',
+        padding: 16 * Dimension.scaleX,
+        borderRadius: 16 * Dimension.scaleX,
+        borderTopRightRadius: 0 * Dimension.scaleX,
+        backgroundColor: Colors.creamLight
     },
     contactText: {
         fontFamily: Fonts.persian.vazirBold,
@@ -45,5 +80,19 @@ export const Styles = StyleSheet.create({
     /** height: 24 */
     largeSpacer: {
         height: 24 * Dimension.scaleY
+    },
+    button: {
+        width: '100%',
+        backgroundColor: Colors.primaryLight
+    },
+    buttonSpacer: {
+        width: 16 * Dimension.scaleX
+    },
+    editImageSafeTouch: {
+        width: '100%',
+        height: 32 * Dimension.scaleY,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#adadadaa'
     }
 })
